6d4cd07f7ca364653ed4231140a633a2db4726ca
Josh <joshthurston92@gmail.com>
2021-10-07 02:08:58 -0600

    finishing up arrays

--- arrarys/customArray.js
+++ arrarys/customArray.js
@@ -5,12 +5,14 @@ class CustomArray {
     this.data = {};
 
   }
+  // O(1)
   push(item) {
     this.data[this.length] = item;
     this.length++;
     return this.data;
   };
 
+  // O(1)
   pop() {
     // let item = this.data[this.length-1];
     // delete item;
@@ -19,6 +21,7 @@ class CustomArray {
     return this.data;
   }
 
+  // O(n)
   insertAt(item, index) {
     for (let i = this.length; i >= index; i--) {
       this.data[i] = this.data[i-1];
@@ -28,6 +31,7 @@ class CustomArray {
     return this.data
   }
 
+  // O(n)
   deleteAt(index) {
     for (let i = index; i < this.length - 1; i++) {
       this.data[i] = this.data[i+1];
@@ -37,10 +41,12 @@ class CustomArray {
     return this.data;
   }
 
+  // O(1)
   getElementAtIndex(index) {
     return this.data[index];
   }
 
+  // O(n)
   indexOf(item) {
     for (let i = 0; i < this.length; i++) {
       if (this.data[i] === item) {
@@ -48,6 +54,47 @@ class CustomArray {
       }
     }
   }
+
+  // O(n)
+  // For returning largest number in array (only works on array of numbers)
+  max() {
+    let largest = this.data[0];
+    for (let i = 1; i < this.length; i++) {
+      if (Number(this.data[i]) === NaN) {
+        throw new Error(`Invalid item ${this.data[i]}; Array must contain only numbers`)
+      }
+      this.data[i] > largest ? largest = this.data[i] : ``;
+    }
+    return largest;
+  }
+
+  // O(n)
+  // Combines all identical values between two arrays into a new one
+  intersect(array) {
+    let output = new CustomArray();
+
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < this.length; j++) {
+        if (array.data[i] === this.data[j]) {
+          output.push(this.data[j]);
+        }
+      }
+    }
+
+    return output;
+  }
+
+  // O(n)
+  reverse() {
+    let output = new CustomArray();
+
+    for (let i = this.length-1; i >= 0; i--) {
+      output.push(this.data[i]);
+    }
+    return output
+  }
+
+
 }
 
 module.exports = CustomArray;
--- arrarys/testingArrays.js
+++ arrarys/testingArrays.js
@@ -2,15 +2,17 @@ const CustomArray = require('./customArray');
 
 let array = new CustomArray();
 
+let testIntersect = new CustomArray();
+
 array.push(10);
 array.push(20);
 array.push(30);
 array.push(40);
 array.push(50);
-console.log(array)
-array.insertAt(60, 2);
-console.log(array.indexOf(30))
+
+
 
 console.log('-'.repeat(99))
+console.log(array.reverse())
 console.log(array)
 console.log('-'.repeat(99))
