8fd378286823b353f1a13a498dd2f9730032765e
Josh <joshthurston92@gmail.com>
2021-10-08 02:25:23 -0600

    more lls

--- linkedLists/LinkedList.js
+++ linkedLists/LinkedList.js
@@ -12,13 +12,70 @@ class LinkedList {
     this.head = head;
   }
 
-  // Todo
-  // addFirst
-  // addLast
-  // deleteFirst
-  // deleteLast
-  // contains
-  // indexOf
+  addFirst(node) {
+    node.next = this.head;
+    this.head = node;
+  }
+
+  addLast(tail) {
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        node.next = tail;
+        return
+      }
+      node = node.next;
+    }
+  }
+
+  deleteFirst() {
+    this.head = this.head.next
+  }
+
+  // needs more work
+  deleteLast() {
+    let node = this.head;
+    while(node) {
+      if (!node.next) {
+        delete node;
+        return;
+      }
+      node = node.next;
+    }
+  }
+
+  contains(data) {
+    let node = this.head;
+    while (node) {
+      if (node.data === data) {
+        return true
+      }
+      node = node.next;
+    }
+    return false;
+  }
+
+  indexOf(data) {
+    let node = this.head;
+    let count = 0;
+    while(node) {
+      if (node.data === data) {
+        return count;
+      }
+      count++
+      node = node.next;
+    }
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
 }
 
 module.exports = {LinkedList, Node};
--- linkedLists/testingLL.js
+++ linkedLists/testingLL.js
@@ -4,6 +4,7 @@ let node1 = new Node('this is node 1');
 let node2 = new Node('and this is node 2');
 let node3 = new Node('node threeee');
 let node4 = new Node('four');
+let node5 = new Node('node five');
 
 node1.next = node2;
 node2.next = node3;
@@ -11,4 +12,11 @@ node3.next = node4;
 
 let list = new LinkedList(node1);
 
-console.log(list.head);
+list.addLast(node5)
+console.log(list);
+
+list.deleteLast()
+// list.deleteFirst();
+console.log(list);
+
+
